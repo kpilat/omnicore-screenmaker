b7c4bdb28f941e1cee08cb3b8cddeafc949e68fc
kpilat <58290934+kpilat@users.noreply.github.com>
2022-02-11 17:00:09 +0100

    Init commit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1 class="test">Tesayfgyguhjh</h1>
 </template>
 
 <script>
@@ -13,14 +14,3 @@ export default {
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
